5af1124252c32e0542af32935dab4f2f4bf827e0
Sokaneil Sieng <sokaneilsieng0505@gmail.com>
2023-09-29 16:50:21 +0200

    chapter 3

--- task02/main.cpp
+++ task02/main.cpp
@@ -3,5 +3,6 @@
 int main(void)
 {
     add_and_print(1, 3);
-    add_and_print(std::string{"hello "}, std::string{"world!"});
+    add_and_print<std::string>(std::string{"hello "}, std::string{"world!"});
+    add_and_print<int, float>(1, 3.1); 
 }
