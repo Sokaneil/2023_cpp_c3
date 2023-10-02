2c86d3e1aa310ee74ec22a91ea7c2804bd51deda
Sokaneil Sieng <sokaneilsieng0505@gmail.com>
2023-10-02 11:42:48 +0200

    chapter 3

--- task04/pos.hpp
+++ task04/pos.hpp
@@ -4,27 +4,29 @@
 #include <iostream>
 #include <string>
 
-template<typename T>
-struct pos_2d {
+template <typename T>
+struct pos_2d
+{
     T x;
     T y;
 };
 
-template<typename T>
+template <typename T>
 std::ostream &operator<<(std::ostream &os, const pos_2d<T> &rhs)
 {
     os << "pos_2d : " << rhs.x << ", " << rhs.y;
     return os;
 }
 
-template<typename T>
-struct pos_3d {
+template <typename T>
+struct pos_3d
+{
     T x;
     T y;
     T z;
 };
 
-template<typename T>
+template <typename T>
 std::ostream &operator<<(std::ostream &os, const pos_3d<T> &rhs)
 {
     os << "pos_3d : " << rhs.x << ", " << rhs.y << ", " << rhs.z;
